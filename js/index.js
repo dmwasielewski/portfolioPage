57c3dbfff8e9539116354e72b422ba0c78234c62
Damian Wasielewski <wasielewski.damian@gmail.com>
2022-07-13 00:37:47 +0100

    menu sticky

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 'use strict';
+let nav = document.querySelector('nav');
+window.addEventListener('scroll', navSticky);
+
+function navSticky() {
+    if (window.scrollY > nav.offsetHeight + 200) {
+        nav.classList.add('navSticky', 'navbar__span');
+    } else {
+        nav.classList.remove('navSticky', 'navbar__span');
+    }
+}
+
+console.log(nav.classList);
